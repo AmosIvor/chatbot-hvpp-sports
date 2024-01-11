b7cdd240892501f857be86697d2d72981adeac40
AmosIvor <trantuanvu00000@gmail.com>
2024-01-11 08:30:07 +0700

    cors policy

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 var app = express();
 
+app.use(cors());
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
